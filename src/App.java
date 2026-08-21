1f82e3ab28e5d4572402fd38d32efce30967e1cd
TE24B-Mia-Hindersson <mia@hindersson.com>
2026-08-21 11:19:57 +0200

    rndnumber gen

--- src/App.java
+++ src/App.java
@@ -1,28 +1,62 @@
+import java.util.Random;
+
 void main()
 {
-    String ålder = IO.readln("Ange din ålder: ");
-    
-    //typkonvertering:
-
-int ålders = Integer.parseInt(ålder);
-
-if(ålders<18){
-    IO.println("du är barn");
-}
-else if(ålders==18){
-    IO.println("du är inte barn anymore");
-}
-else{
-    IO.println("wow");
-}
-
-switch(ålders){
-    case 18 -> IO.println("du är inte barn anymore typ");
-    default -> IO.println("uh ok");
-}
+Random rand = new Random();
+int randomnr = rand.nextInt(9) + 1;
+IO.println("du ska gissa ett nr från 1-10, du har 3 försök:");
 int varv = 0;
-while (varv<10){
-    IO.println("varv nr "+varv);
-    varv = varv+1; 
+while (varv<3)
+{
+String answer = IO.readln("Ange din gissning: ");
+ int siif = Integer.parseInt(answer);
+if ( siif == randomnr)
+{
+    IO.println("du fick rätt!!");
+}
+else if (siif != randomnr) 
+    {
+    IO.println("du fick fel!");
+} 
+else {
+     IO.println("fuck you");
+}
+varv ++;
 }
 }
+
+
+
+
+
+
+
+// void main()
+// {
+//     String ålder = IO.readln("Ange din ålder: ");
+    
+//     //typkonvertering:
+
+// int ålders = Integer.parseInt(ålder);
+
+// if(ålders<18){
+//     IO.println("du är barn");
+// }
+// else if(ålders==18){
+//     IO.println("du är inte barn anymore");
+// }
+// else{
+//     IO.println("wow");
+// }
+
+// switch(ålders){
+//     case 18 -> IO.println("du är inte barn anymore typ");
+//     default -> IO.println("uh ok");
+// }
+// int varv = 0;
+// while (varv<10){
+//     IO.println("varv nr "+varv);
+//     varv = varv+1; 
+// }
+// }
+
